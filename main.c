7285809e4725b2d9769c3c37d9099a0c84e07e35
myfoolish-dev <roydennelson.k.dev@gmail.com>
2024-05-16 17:38:32 -1000

    un

--- main.c
+++ main.c
@@ -5,16 +5,21 @@ void example(int *a);
 int main()
 {
     int a = 10;
+
+    // Reference
     int *pA = &a;
 
     printf("Value: %i\n", a);
 
+    // Passing a reference
     example(pA);
     
     printf("Value: %i\n", a);
     return 0;
 }
+// When passing a pointer/reference, you must specify that you are passing a pointer/reference.
 void example(int *a)
 {
+    // Dereference
     *a = 15;
 }
